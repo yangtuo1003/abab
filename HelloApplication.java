668d8bfb33ea3ee0139f6446a9b5cfad528d73de
yangtuo1003 <3127537978@qq.com>
2025-03-25 17:17:33 +0800

    Add files via upload

---
+++ HelloApplication.java
@@ -0,0 +1,10 @@
+package com.wx.demo;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+    ab
+}
